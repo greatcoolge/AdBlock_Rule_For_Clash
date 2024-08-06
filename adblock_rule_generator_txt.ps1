593d5f2997e26700d332804acfbfc8e24d0fbd78
REIJI007 <miyajimareji007@gmail.com>
2024-08-07 00:30:44 +0800

    Update adblock_rule_generator_txt.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator_txt.ps1
+++ adblock_rule_generator_txt.ps1
@@ -104,7 +104,9 @@ $txtContent = @"
 # Title: AdBlock_Rule_For_Clash
 # Description: 适用于Clash的域名拦截rule-providers，每20分钟更新一次，确保即时同步上游减少误杀
 # Homepage: https://github.com/REIJI007/AdBlock_Rule_For_Clash
-# Licence: https://github.com/REIJI007/AdBlock_Rule_For_Clash/blob/main/LICENSE
+# LICENSE1： https://github.com/REIJI007/AdBlock_Rule_For_Clash/blob/main/LICENSE-GPL3.0
+# LICENSE2： https://github.com/REIJI007/AdBlock_Rule_For_Clash/blob/main/LICENSE-CC%20BY-NC-SA%204.0
+
 # Generated AdBlock rules
 # Total entries: $ruleCount
 
