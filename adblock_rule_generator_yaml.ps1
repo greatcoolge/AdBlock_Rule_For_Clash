9067f5e376ab0bc237ebfef367b67bd4858979a3
REIJI007 <miyajimareji007@gmail.com>
2024-08-23 09:54:00 +0800

    Update adblock_rule_generator_yaml.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator_yaml.ps1
+++ adblock_rule_generator_yaml.ps1
@@ -8,15 +8,45 @@ $urlList = @(
     "https://anti-ad.net/easylist.txt",
     "https://easylist-downloads.adblockplus.org/easylist.txt",
     "https://easylist-downloads.adblockplus.org/easylistchina.txt",
-    "https://easylist-downloads.adblockplus.org/easyprivacy.txt"
+    "https://easylist-downloads.adblockplus.org/easyprivacy.txt",
+    "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt",
+    "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt",
+    "https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_plus.txt",
+    "https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_privacy.txt",
+    "https://raw.githubusercontent.com/Cats-Team/AdRules/main/adblock_plus.txt",
+    "https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt",
+    "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt",
+    "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockfilters.txt",
+    "https://raw.githubusercontent.com/8680/GOODBYEADS/master/rules.txt",
+    "https://raw.githubusercontent.com/8680/GOODBYEADS/master/dns.txt",
+    "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt",
+    "https://raw.githubusercontent.com/Bibaiji/ad-rules/main/rule/ad-rules.txt",
+    "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt",
+    "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt",
+    "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters-mobile.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_17_TrackParam/filter.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_4_Social/filter.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_14_Annoyances/filter.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_10_Useful/filter.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_7_Japanese/filter.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_11_Mobile/filter.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_15_DnsFilter/filter.txt",
+    "https://raw.githubusercontent.com/Lynricsy/HyperADRules/master/rules.txt",
+    "https://raw.githubusercontent.com/Lynricsy/HyperADRules/master/dns.txt",
+    "https://raw.githubusercontent.com/guandasheng/adguardhome/main/rule/all.txt",
+    "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt",
+    "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt",
+    "https://raw.githubusercontent.com/superbigsteam/adguardhomeguiz/main/rule/all.txt"
 )
 
 # 日志文件路径
 $logFilePath = "$PSScriptRoot/adblock_log.txt"
 
-# 创建两个HashSet来存储唯一的规则
-$domainRules = [System.Collections.Generic.HashSet[string]]::new()
-$domainSuffixRules = [System.Collections.Generic.HashSet[string]]::new()
+# 创建一个HashSet来存储唯一的规则
+$uniqueRules = [System.Collections.Generic.HashSet[string]]::new()
 
 # 创建WebClient对象用于下载URL内容
 $webClient = New-Object System.Net.WebClient
@@ -33,38 +63,25 @@ foreach ($url in $urlList) {
 
         foreach ($line in $lines) 
         {
-            # 清除空格和转义字符
-            $line = $line.Trim()
-
-            # Adblock/Easylist 格式的完整域名 (添加 DOMAIN 前缀)
+            # 匹配 Adblock/Easylist 格式的规则
             if ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^$') {
                 $domain = $Matches[1]
-                $domainRules.Add("DOMAIN,$domain") | Out-Null
+                $uniqueRules.Add($domain) | Out-Null
             }
-            # Adblock/Easylist 格式的子域名 (添加 DOMAIN-SUFFIX 前缀)
-            elseif ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$') {
-                $domain = $Matches[1]
-                $domainSuffixRules.Add("DOMAIN-SUFFIX,$domain") | Out-Null
-            }
-            # Hosts 文件格式的完整域名 (添加 DOMAIN 前缀)
+            # 匹配 Hosts 文件格式的规则
             elseif ($line -match '^(0\.0\.0\.0|127\.0\.0\.1)\s+([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$') {
                 $domain = $Matches[2]
-                $domainRules.Add("DOMAIN,$domain") | Out-Null
+                $uniqueRules.Add($domain) | Out-Null
             }
-            # Hosts 文件格式的子域名 (添加 DOMAIN-SUFFIX 前缀)
-            elseif ($line -match '^(0\.0\.0\.0|127\.0\.0\.1)\s+([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$') {
-                $domain = $Matches[2]
-                $domainSuffixRules.Add("DOMAIN-SUFFIX,$domain") | Out-Null
-            }
-            # Dnsmasq/AdGuard 格式的完整域名 (添加 DOMAIN 前缀)
+            # 匹配 Dnsmasq/AdGuard 格式的规则
             elseif ($line -match '^address=/([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/$') {
                 $domain = $Matches[1]
-                $domainRules.Add("DOMAIN,$domain") | Out-Null
+                $uniqueRules.Add($domain) | Out-Null
             }
-            # Dnsmasq/AdGuard 格式的子域名 (添加 DOMAIN-SUFFIX 前缀)
-            elseif ($line -match '^address=/([a-zA-Z0-9-]+\.[a-zA-Z]{2,})/$') {
+            # 匹配通配符匹配格式的规则
+            elseif ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^$') {
                 $domain = $Matches[1]
-                $domainSuffixRules.Add("DOMAIN-SUFFIX,$domain") | Out-Null
+                $uniqueRules.Add($domain) | Out-Null
             }
         }
     }
@@ -75,14 +92,10 @@ foreach ($url in $urlList) {
 }
 
 # 对规则进行排序并添加前缀
-$formattedDomainRules = $domainRules | Sort-Object | ForEach-Object { "  - $_" }
-$formattedDomainSuffixRules = $domainSuffixRules | Sort-Object | ForEach-Object { "  - $_" }
-
-# 合并规则列表
-$allFormattedRules = $formattedDomainRules + $formattedDomainSuffixRules
+$formattedRules = $uniqueRules | Sort-Object | ForEach-Object { "  - '+.$_'  " }
 
 # 统计生成的规则条目数量
-$ruleCount = $allFormattedRules.Count
+$ruleCount = $uniqueRules.Count
 
 # 获取当前东八区时间
 $timeZoneInfo = [System.TimeZoneInfo]::FindSystemTimeZoneById("China Standard Time")
@@ -101,7 +114,7 @@ $textContent = @"
 # Total entries: $ruleCount
 
 payload:
-$($allFormattedRules -join "`n")
+$($formattedRules -join "`n")
 "@
 
 # 定义输出文件路径
