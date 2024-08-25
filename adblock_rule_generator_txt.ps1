8ce5d437135f1f0dfe2281080321583975d3a4d8
REIJI007 <miyajimareji007@gmail.com>
2024-08-25 15:34:53 +0800

    Update adblock_rule_generator_txt.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator_txt.ps1
+++ adblock_rule_generator_txt.ps1
@@ -88,6 +88,7 @@ $urlList = @(
 "https://raw.githubusercontent.com/brave/adblock-lists/master/brave-lists/brave-firstparty.txt",
 "https://raw.githubusercontent.com/brave/adblock-lists/master/brave-lists/brave-firstparty-cname.txt",
 "https://raw.githubusercontent.com/brave/adblock-lists/master/brave-unbreak.txt"
+
 )
 
 # 日志文件路径
@@ -99,22 +100,7 @@ $uniqueRules = [System.Collections.Generic.HashSet[string]]::new()
 # 创建WebClient对象用于下载URL内容
 $webClient = New-Object System.Net.WebClient
 $webClient.Encoding = [System.Text.Encoding]::UTF8
-$webClient.Proxy = $null
-
-# 定义域名验证函数
-function IsValidDomain {
-    param (
-        [string]$domain
-    )
-
-    # 使用正则表达式验证域名
-    if ($domain -match '^(?!\-)([a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,}$') {
-        return $true
-    } else {
-        return $false
-    }
-}
-
+$webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
 foreach ($url in $urlList) {
     Write-Host "正在处理: $url"
@@ -123,101 +109,34 @@ foreach ($url in $urlList) {
     {
         $content = $webClient.DownloadString($url)
         $lines = $content -split "`n"
+
         foreach ($line in $lines) 
         {
-            $line = $line.Trim()
-            
-            # 忽略空行和注释
-            if ([string]::IsNullOrWhiteSpace($line) -or $line.StartsWith("#")) {
+            # 排除例外规则
+            if ($line -match '^@@') {
                 continue
             }
 
-            # 直接忽略以@@开头的规则
-            if ($line -match '^@@\|\|') {
-                continue
+            # 匹配 Adblock/Easylist 格式的规则
+            if ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^$') {
+                $domain = $Matches[1]
+                $uniqueRules.Add($domain) | Out-Null
             }
-            # 处理形如||example.com^的域名，加上前缀'+.和后缀'
+            # 匹配 Hosts 文件格式的规则
+            elseif ($line -match '^(0\.0\.0\.0|127\.0\.0\.1)\s+([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$') {
+                $domain = $Matches[2]
+                $uniqueRules.Add($domain) | Out-Null
+            }
+            # 匹配 Dnsmasq/AdGuard 格式的规则
+            elseif ($line -match '^address=/([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/$') {
+                $domain = $Matches[1]
+                $uniqueRules.Add($domain) | Out-Null
+            }
+            # 匹配通配符匹配格式的规则
             elseif ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^$') {
                 $domain = $Matches[1]
-                if (IsValidDomain($domain)) {
-                    $uniqueRules.Add("- '+.$domain'") | Out-Null
-                }
+                $uniqueRules.Add($domain) | Out-Null
             }
-            # 处理形如||*.example.com^的域名，加上前缀'+.和后缀'
-            elseif ($line -match '^\|\|\*\.([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^$') {
-                $domain = $Matches[1]
-                if (IsValidDomain($domain)) {
-                    $uniqueRules.Add("- '+.$domain'") | Out-Null
-                }
-            }
-            # 处理形如example.com的域名，加上前缀'+.和后缀'
-            elseif ($line -match '^([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$') {
-                $domain = $Matches[1]
-                if (IsValidDomain($domain)) {
-                    $uniqueRules.Add("- '+.$domain'") | Out-Null
-                }
-            }
-            # 处理形如||example.com^$all的域名，加上前缀'+.和后缀'
-            elseif ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^\$all$') {
-                $domain = $Matches[1]
-                if (IsValidDomain($domain)) {
-                    $uniqueRules.Add("- '+.$domain'") | Out-Null
-                }
-            }
-            # 处理形如/^[a-z0-9-]+\.example\.com$/的域名，加上前缀'+.和后缀'
-            elseif ($line -match '^/\^[a-z0-9-]+\.([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\$/$') {
-                $domain = $Matches[1]
-                if (IsValidDomain($domain)) {
-                    $uniqueRules.Add("- '+.$domain'") | Out-Null
-                }
-            }
-            # 处理形如address=/example.com/0.0.0.0的域名，加上前缀'+.和后缀'
-            elseif ($line -match '^address=/([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/0\.0\.0\.0$') {
-                $domain = $Matches[1]
-                if (IsValidDomain($domain)) {
-                    $uniqueRules.Add("- '+.$domain'") | Out-Null
-                }
-            }
-            # 处理形如/^example\.com$/的域名，加上前缀'和后缀'
-            elseif ($line -match '^/\^([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\$/$') {
-                $domain = $Matches[1]
-                if (IsValidDomain($domain)) {
-                    $uniqueRules.Add("- '$domain'") | Out-Null
-                }
-            }
-            # 处理形如/^([a-z0-9-]+\.)?example\.com$/的域名，加上前缀'和后缀'
-            elseif ($line -match '^/\^([a-z0-9-]+\.)?\s*([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\$/$') {
-                $domain = $Matches[2]
-                if (IsValidDomain($domain)) {
-                    $uniqueRules.Add("- '$domain'") | Out-Null
-                }
-            }
-            # 处理形如address=/example.com/127.0.0.1的域名，加上前缀'和后缀'
-            elseif ($line -match '^address=/([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/127\.0\.0\.1$') {
-                $domain = $Matches[1]
-                if (IsValidDomain($domain)) {
-                    $uniqueRules.Add("- '$domain'") | Out-Null
-                }
-            }
-            # 处理形如127.0.0.1 example.com的域名，加上前缀'和后缀'
-            elseif ($line -match '^127\.0\.0\.1\s+([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$') {
-                $domain = $Matches[1]
-                if (IsValidDomain($domain)) {
-                    $uniqueRules.Add("- '$domain'") | Out-Null
-                }
-            }
-            # 处理形如0.0.0.0 example.com的域名，加上前缀'和后缀'
-            elseif ($line -match '^0\.0\.0\.0\s+([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$') {
-                $domain = $Matches[1]
-                if (IsValidDomain($domain)) {
-                    $uniqueRules.Add("- '$domain'") | Out-Null
-                }
-            }
-            # 忽略其他形如||example.com^$的域名
-            elseif ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^\$') {
-                continue
-            }
-            # 其余不符合上述处理逻辑的条目一律忽略
         }
     }
     catch {
@@ -228,16 +147,14 @@ foreach ($url in $urlList) {
 
 
 
-# 对规则进行排序
-$formattedRules = $uniqueRules | Sort-Object | ForEach-Object { "$_" }
+# 对规则进行排序并格式化
+$formattedRules = $uniqueRules | Sort-Object | ForEach-Object {"'+.$_'"}
 
 # 统计生成的规则条目数量
 $ruleCount = $uniqueRules.Count
 
-# 获取当前东八区时间
-$timeZoneInfo = [System.TimeZoneInfo]::FindSystemTimeZoneById("China Standard Time")
-$localTime = [System.TimeZoneInfo]::ConvertTime([System.DateTime]::UtcNow, $timeZoneInfo)
-$generatedTime = $localTime.ToString("yyyy-MM-dd HH:mm:ss")
+# 获取当前时间并转换为东八区时间
+$generationTime = (Get-Date).ToUniversalTime().AddHours(8).ToString("yyyy-MM-dd HH:mm:ss")
 
 # 创建文本格式的字符串
 $textContent = @"
@@ -246,11 +163,10 @@ $textContent = @"
 # Homepage: https://github.com/REIJI007/AdBlock_Rule_For_Clash
 # LICENSE1：https://github.com/REIJI007/AdBlock_Rule_For_Clash/blob/main/LICENSE-GPL3.0
 # LICENSE2：https://github.com/REIJI007/AdBlock_Rule_For_Clash/blob/main/LICENSE-CC%20BY-NC-SA%204.0
+# Generated on: $generationTime
 # Generated AdBlock rules
-# Generated on: $generatedTime (GMT+8)
 # Total entries: $ruleCount
 
-payload:
 $($formattedRules -join "`n")
 "@
 
@@ -260,6 +176,7 @@ $textContent | Out-File -FilePath $outputPath -Encoding utf8
 
 # 输出生成的有效规则总数
 Write-Host "生成的有效规则总数: $ruleCount"
-Add-Content -Path $logFilePath -Value "生成的有效规则总数: $ruleCount"
+Add-Content -Path $logFilePath -Value "Total entries: $ruleCount"
+Add-Content -Path $logFilePath -Value "Generated at: $generationTime"
 
 Pause
